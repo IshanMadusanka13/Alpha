8ebcb926722bd876638972b2465cfb53af6ceff4
Ishan Madusanka <madusankaishan13@gmail.com>
2025-06-26 08:47:20 +0530

    Added Project Demos

--- src/constants/index.js
+++ src/constants/index.js
@@ -47,7 +47,7 @@ export const projects = [
 		id: "saloncapta",
 		name: "Salon Capta",
 		type: "Web App",
-		status: "Completed",
+		status: "Ongoing",
 		image: 'projects/capta.png',
 		tech: "Next JS, Tailwind CSS, Spring Boot",
 		description: "Salon Capta is a modern web-based salon management system designed to streamline both customer and owner experiences. Customers can easily browse services and make online bookings, while salon owners manage appointments, services, and customer data through a dedicated dashboard. Built with Next.js, Tailwind CSS, and Spring Boot, the platform integrates Stripe for secure online payments and provides a responsive, user-friendly interface across devices.",
@@ -61,6 +61,41 @@ export const projects = [
 			"Service management",
 			"Responsive design"
 		]
+	},
+		{
+		id: "skillSync",
+		name: "SkillSync",
+		type: "Web App",
+		status: "Completed",
+		image: 'projects/skillSync.png',
+		tech: "Vite Js, Spring Boot, PostgreSQL",
+		description: "SkillSync is an innovative social media platform designed for individuals passionate about learning and sharing skills, creating a dynamic online community where users can connect,collaborate, and grow together. Built with Vite.js for a fast and responsive frontend, Spring Boot for robust backend services, and PostgreSQL for reliable data management, SkillSync offers a seamless user experience. The platform features user profiles, skill sharing, and community engagement tools, all while ensuring data security and performance.",
+		projectUrl: "https://github.com/IshanMadusanka13/skillconnect",
+		demoVideo: "/assets/projects/SkillSync-demo.mp4",
+		technologies: ["Vite Js", "Spring Boot", "PostgreSQL"],
+		features: [
+			"User profiles",
+			"Skill sharing",
+			"Community engagement tools",
+			"Data security and performance"
+		]
+	},
+	{
+		id: "lioc",
+		name: "LiOC",
+		type: "Web App",
+		status: "Completed",
+		image: 'projects/lioc.png',
+		tech: "React Js, Spring Boot, MySQL",
+		description: "LiOC is a simple fuel station management system designed to streamline operations for fuel stations. It provides a comprehensive platform for managing fuel sales, inventory, and supplier management. Built with React.js for a dynamic user interface, Spring Boot for robust backend services, and MySQL for reliable data storage, LiOC ensures efficient management of fuel station operations.",
+		projectUrl: "https://github.com/IshanMadusanka13/LIOC",
+		demoVideo: "/assets/projects/LIOC-demo.mp4",
+		technologies: ["React Js", "Spring Boot", "MySQL"],
+		features: [
+			"User profiles",
+			"Fuel sales management",
+			"Inventory tracking",
+			"Supplier management"
+		]
 	}
-	
 ];
